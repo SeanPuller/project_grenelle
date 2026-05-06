a9ae1b8649b95a4d37229b719e603a89f6e2cac5
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2026-05-06 17:24:15 +0200

    update icon

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.43';
+const APP_VERSION = '0.44';
 document.addEventListener('DOMContentLoaded', () => {
 	const mainContent = document.getElementById('main-content');
 	const navLinks = document.querySelectorAll('.nav-link');
