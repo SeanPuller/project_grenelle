65e9fc02cf502c675b10d99fbb9679ccac3b2a28
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2026-05-06 19:12:34 +0200

    ios home screen support

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.44';
+const APP_VERSION = '0.45';
 document.addEventListener('DOMContentLoaded', () => {
 	const mainContent = document.getElementById('main-content');
 	const navLinks = document.querySelectorAll('.nav-link');
