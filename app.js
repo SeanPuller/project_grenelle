267e561acadc82b1d9a551f8c76121c6df10b694
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2026-05-08 12:44:12 +0200

    remove italic from logo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.51';
+const APP_VERSION = '0.52';
 document.addEventListener('DOMContentLoaded', () => {
 	const mainContent = document.getElementById('main-content');
 	const navLinks = document.querySelectorAll('.nav-link');
