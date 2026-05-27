e0cadeccf6696a09842ddbbeb5612e77e2c75ad3
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2026-05-27 19:52:36 +0200

    remove the reset colors button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.66';
+const APP_VERSION = '0.67';
 document.addEventListener('DOMContentLoaded', () => {
 	const mainContent = document.getElementById('main-content');
 	const navLinks = document.querySelectorAll('.nav-link');
