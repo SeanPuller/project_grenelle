e7d6dae9e1b6fdcd37e123e42122a7a4454d1a04
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2026-05-28 15:19:43 +0200

    add search bar to exercises screen

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.68';
+const APP_VERSION = '0.69';
 document.addEventListener('DOMContentLoaded', () => {
 	const mainContent = document.getElementById('main-content');
 	const navLinks = document.querySelectorAll('.nav-link');
